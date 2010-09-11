4f702e789126de512eba3953bd62923ef668d5f2
defagos <defagos@gmail.com>
2010-09-11 16:48:42 +0200

    Cleaned up options

--- make-fmwk.sh
+++ make-fmwk.sh
@@ -3,95 +3,55 @@
 # Constants
 VERSION_NBR=1.0
 SCRIPT_NAME=`basename $0`
-
 # Directory from which the script is executed
-EXECUTION_DIR=`pwd`
+EXECUTION_DIR=`pwd`                     
+# Directory where all frameworks are saved (for all possible configurations)
+FRAMEWORK_COMMON_OUTPUT_DIR="$EXECUTION_DIR/build/framework"
+
+# Global variables
+param_copy_source_files=false
 
 # User manual
 usage() {
     echo ""
     echo "This script packages a static library project into a reusable .framework"
     echo "for iOS projects. It must be launched from the directory containing the"
-    echo ".xcodeproj directory, and will generate framework bundles in the"
-    echo "build/framework directory."
+    echo ".xcodeproj of a static library project, and will generate framework bundles"
+    echo "under build/framework"
     echo ""
-    echo "Frameworks are built on a per-configuration basis. The name of the"
-    echo "configuration which has been used to generate a framework will be appended"
-    echo "to its name."
+    echo "Frameworks are built on a per-configuration basis since a universal binary"
+    echo "file can contain at most one binary per platform. The configuration which"
+    echo "has been used to generate a framework is appended to its name."
     echo ""
-    echo "Usage: $SCRIPT_NAME [-s] [-a] [-c configuration_name] [-v] [-h]"
+    echo "Usage: $SCRIPT_NAME [-s] [-v] [-h] configuration_name public_headers_file"
+    echo ""
+    echo "Mandatory parameters:"
+    echo "   configuration_name     The name of the configuration to use"
+    echo "   public_headers_file    A file containing the list of headers use to"
+    echo "                          create the framework public interface, one per"
+    echo "                          line:"
+    echo "                              header1.h"
+    echo "                              header2.h"
+    echo "                                 ..."
+    echo "                              headerN.h"
     echo ""
     echo "Options:"
-    echo "   -a:                 Build frameworks for all configurations defined"
-    echo "                       by the project"
-    echo "   -c:                 Build the framework for a specific configuration."
-    echo "                       Ignored if -a is used"
-    echo "   -h:                 Display this documentation"
-    echo "   -s:                 Add source code to the bundle file. Useful"
-    echo "                       for debug binaries"
-    echo "   -v:                 Print the version number"
+    echo "   -h:                    Display this documentation"
+    echo "   -s:                    Add the complete source code to the bundle file."
+    echo "                          Useful for frameworks compiled with debug symbols"
+    echo "   -v:                    Print the version number"
     echo ""
-}
-
-# Function for generating the framework for a single configuration
-# @param $1 configuration name
-generate_framework_for_configuration() {
-    configuration_name="$1"
-    
-    # Framework name matches project name
-    # TODO: Read from xcodeproj; move outside of this function
-    PROJECT_NAME="MyProject"
-    
-    # Directory where all frameworks will be saved (for all possible configurations)
-    framework_common_output_dir="$EXECUTION_DIR/build/framework"
-    
-    # Framework directory ( 
-    framework_output_dir="$framework_common_output_dir/$PROJECT_NAME-$configuration_name.framework"
-    
-    # Begin framework creation
-    echo "Creating framework..."
-    
-    # Cleanup framework if it already existed
-    if [ -d "$framework_output_dir" ]; then
-        rm -rf "$framework_output_dir"
-    fi
-    
-    # Create the main framework directory
-    mkdir -p "$framework_output_dir"
-    
-    # Create the framework internal structure and symbolic links
-    # (see http://developer.apple.com/library/mac/#documentation/MacOSX/Conceptual/BPFrameworks/Tasks/CreatingFrameworks.htm)
-    mkdir -p "$framework_output_dir/Versions/A/Headers"
-    mkdir -p "$framework_output_dir/Versions/A/Resources"
-    
-    # Symbolic links. The link target has to be given as a relative path to where the link file is located,
-    # otherwise links will break when the framework is moved
-    ln -s "./A" "$framework_output_dir/Versions/Current"
-    ln -s "./Versions/Current/Headers" "$framework_output_dir/Headers"
-    ln -s "./Versions/Current/Resources" "$framework_output_dir/Resources" 
-    ln -s "./Versions/Current/$PROJECT_NAME" "$framework_output_dir/$PROJECT_NAME"
-    
-    # TODO: Find .a file names for assembling into universal file
 }
 
 # Processing command-line parameters
-while getopts ac:hsv OPT; do
+while getopts hsv OPT; do
     case "$OPT" in
-        a)
-            # TODO: Implement
-            exit 0
-            ;;
-        c)
-            # TODO: Move outside this case
-            generate_framework_for_configuration "$OPTARG"
-            exit 0
-            ;;
         h)
             usage
             exit 0
             ;;
         s)
-            # TODO: Implement
+            param_copy_source_files=true
             exit 0
             ;;
         v)
@@ -105,4 +65,57 @@ while getopts ac:hsv OPT; do
     esac
 done
 
+# Read the remaining mandatory parameters
+shift `expr $OPTIND - 1`
+for arg in "$@"; do
+    if [ -z "$configuration_name" ]; then
+        configuration_name="$arg"
+    elif [ -z "$public_headers_file" ]; then
+        public_headers_file="$arg"
+    else
+       usage
+       exit 1
+    fi
+done
 
+# The last argument is still not filled, incomplete command line
+if [ -z "$public_headers_file" ]; then
+    usage
+    exit 1
+fi
+
+# Framework name matches project name
+# TODO: Read from xcodeproj; move outside of this function
+PROJECT_NAME="MyProject"
+
+# Framework directory ( 
+framework_output_dir="$FRAMEWORK_COMMON_OUTPUT_DIR/$PROJECT_NAME-$configuration_name.framework"
+
+# Begin framework creation
+echo "Creating framework for configuration $configuration_name..."
+
+# Cleanup framework if it already existed
+if [ -d "$framework_output_dir" ]; then
+    echo "Framework already exists for configuration $configuration_name. Cleaning up..."
+    rm -rf "$framework_output_dir"
+fi
+
+# Create the main framework directory
+mkdir -p "$framework_output_dir"
+
+# Create the framework internal structure and symbolic links
+# (see http://developer.apple.com/library/mac/#documentation/MacOSX/Conceptual/BPFrameworks/Tasks/CreatingFrameworks.htm)
+mkdir -p "$framework_output_dir/Versions/A/Headers"
+mkdir -p "$framework_output_dir/Versions/A/Resources"
+
+# Symbolic links. The link target has to be given as a relative path to where the link file is located,
+# otherwise links will break when the framework is moved
+ln -s "./A" "$framework_output_dir/Versions/Current"
+ln -s "./Versions/Current/Headers" "$framework_output_dir/Headers"
+ln -s "./Versions/Current/Resources" "$framework_output_dir/Resources" 
+ln -s "./Versions/Current/$PROJECT_NAME" "$framework_output_dir/$PROJECT_NAME"
+
+# TODO: Find .a file names for assembling into universal file
+
+# Done
+echo "Framework created for configuration $configuration_name"
