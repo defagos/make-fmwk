dcc9d4642769dcf18c99a349af786b46695867a3
defagos <defagos@gmail.com>
2010-11-01 17:46:16 +0100

    Updating example to match new script abilities

--- example/PrefixClientApp/Classes/PrefixClientAppAppDelegate.m
+++ example/PrefixClientApp/Classes/PrefixClientAppAppDelegate.m
@@ -8,20 +8,7 @@
 
 #import "PrefixClientAppAppDelegate.h"
 
-// Framework public headers are imported using the < > syntax. We here use the framework obtained by compiling
-// the PrefixLibrary static library project using the Debug configuration and the public headers defined in the
-// PrefixLibrary publicHeaders.txt file. The framework is obtained by running the make-fmwk.sh command from
-// the PrefixLibrary main directory:
-//                 /path/where/the/script/is/located/make-fmwk.sh Debug publicHeaders.txt
-// The framework is saved into the PrefixLibrary/build/framework directory.
-//
-// You will probably have to restore the link between this project and the framework (the framework should appear
-// in red in the project explorer):
-//   1) In the project explorer tree, remove the framework reference under "Frameworks", and add the .framework
-//      you have just created.
-//   2) Restore the resource links by emptying the "Framework Resources/PrefixLibrary-Debug" folder of the project
-//      explorer tree, and by adding the contents of the Resources folder of the .framework bundle you have just
-//      created.
+// Framework public headers are imported using the < > syntax
 #import <PrefixLibrary/SimpleViewController.h>
 
 @interface PrefixClientAppAppDelegate ()
@@ -53,11 +40,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {    
-    // This is a framework object using its own resources (xib, image, localized string). These resources do not get
-    // copied into the client application bundle just by adding the framework. We need to add the resource files
-    // explicitly to the project (see "Framework Resources" folder in the project explorer). This way the framework
-    // resources get copied into the client application bundle (see "Copy Bundle Resources" under the main target
-    // defined for this project), and everything works!
+    // Create an object from the library, here a view controller
     self.simpleViewController = [[[SimpleViewController alloc] init] autorelease];
 	[self.window addSubview:self.simpleViewController.view];
     
--- example/PrefixLibrary/SimpleViewController.m
+++ example/PrefixLibrary/SimpleViewController.m
@@ -14,9 +14,9 @@
 
 - (id)init
 {
-    // Bundle is nil, i.e. main bundle. The PrefixLibrary framework is designed in such a way that its use
-    // requires the client to merge its resources into the main bundle. To avoid name conflicts we add a 
-    // prefix to all framework resources
+    // Bundle is here nil, i.e. the main bundle (the only one we can access on iOS). To avoid name
+    // conflicts when framework resources are merged with application resources (and maybe resources
+    // stemming from other frameworks), resources are prefixed with the library name.
     if (self = [super initWithNibName:@"PrefixLibrary_SimpleViewController" bundle:nil]) {
         
     }
@@ -34,10 +34,8 @@
 
 - (void)viewDidLoad
 {
-    // As for any resources of the PrefixLibrary framework, localization files end up merged into the main
-    // bundle. We must use a separate localization file with a prefix for avoiding conflicts with the usual
-    // Localizable.strings file we can probably expect client applications to use. This of course means
-    // that we must use the appropriate macro for getting the localized string.
+    // Localization files are resources and must be prefixed (see above). This of course means that we must 
+    // use the appropriate macro for getting the localized string.
     self.label.text = NSLocalizedStringFromTable(@"HelloWorld", @"PrefixLibrary_Localizable", @"Hello, World!");
     
     // As for any resource files, images must bear the framework prefix as well
