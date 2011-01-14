097f68d9f7d3c1ae465cf3c049807ec37a4f68d3
Samuel Défago <defagos@gmail.com>
2011-01-14 10:58:34 +0100

    Added file locks to prevent accidental .staticframework changes

--- make-fmwk.sh
+++ make-fmwk.sh
@@ -324,6 +324,10 @@ framework_output_dir="$output_dir/$framework_full_name.staticframework"
 # Cleanup framework if it already existed
 if [ -d "$framework_output_dir" ]; then
     echo "Framework already exists. Cleaning up first..."
+    
+    # Remove locks
+    find "$framework_output_dir" -exec SetFile -a l {} \;
+    
     rm -rf "$framework_output_dir"
 fi
 
@@ -643,6 +647,9 @@ fi
 
 echo "</dict>" >> "$manifest_file"
 echo "</plist>" >> "$manifest_file"
+
+# Lock all .staticframework files to prevent the user from accidentally editing them within Xcode
+find "$framework_output_dir" -exec SetFile -a L {} \;
 
 # Done
 echo "Done."
