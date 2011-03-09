21df9ee3dbe9f76c7c30b7e3857a38414ef33232
Samuel Défago <defagos@gmail.com>
2011-03-09 20:58:37 +0100

    Slightly less loose binary lookup for lipo command. Fixed .gitignore

--- make-fmwk.sh
+++ make-fmwk.sh
@@ -467,8 +467,7 @@ mkdir -p "$headers_output_dir"
 # Packing static libraries as universal binaries. For the linker to be able to find the static unversal binaries in the 
 # framework bundle, the universal binaries must bear the exact same name as the framework
 echo "Packing binaries..."
-lipo -create "$BUILD_DIR/$configuration_name-iphonesimulator/"* \
-    "$BUILD_DIR/$configuration_name-iphoneos/"* \
+lipo -create "$BUILD_DIR/$configuration_name-iphonesimulator/"*.a "$BUILD_DIR/$configuration_name-iphoneos/"*.a \
     -o "$dot_framework_output_dir/$framework_name"
 
 # Load the public header file list into an array (remove blank lines if anys)
